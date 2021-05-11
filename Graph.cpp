9d8a46f1a1539afae97c9a7265f0092215f63754
cvancory1 <cvancory1@gmail.com>
2021-05-11 16:37:09 -0400

    same

--- Graph.cpp
+++ Graph.cpp
@@ -639,9 +639,10 @@ void Graph<T>::color(){
 
     }
     cout<<endl;
-    subGraphs[arrSize-2].print();
+    subGraphs[arrSize-1].print();
+    cout<<"The chromatic number of the graph is "<< X[arrSize-1]<<endl;
     cout<<"The chromatic number of the graph is "<< X[arrSize-2]<<endl;
-
+    cout<<"The chromatic number of the graph is "<< X[arrSize]<<endl;
 
 }
 
